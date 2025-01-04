8ddba5fff61c77febd33e22f200b0039994684e2
Patel Harsh <harshpatel8320@gmail.com>
2025-01-04 18:37:39 +0530

    Setup Cloudinary and create APIs

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
-// const express = require("express")
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
@@ -8,9 +9,18 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
-  console.log(`server is running on ${PORT}`);
-  connectDB()
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
 });
