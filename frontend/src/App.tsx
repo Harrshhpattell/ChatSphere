75c34b4d7454c746cddd0b68a4c7c35c079e3a07
Patel Harsh <harshpatel8320@gmail.com>
2025-01-19 17:28:53 +0530

    frontend project setup
    - vite ( typescript )
    - tailwindcss
    - shadcn ui
    - lucid ( icons )

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from "@/components/ui/button"
+import { MailOpen } from 'lucide-react'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -22,12 +24,13 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Button size="lg"><MailOpen /> Login with Email</Button>
     </>
   )
 }
